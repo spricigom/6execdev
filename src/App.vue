c2e196e6278a940f489ff3aea0fbea97477d07c5
spricigom <emilispricigo.ifc@gmail.com>
2024-03-15 15:17:30 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue'
+
+const nome = ref('')
+const idade = ref('')
+const email = ref('')
+</script>
+<template>
+  <input type="text" v-model="nome" />
+  <p>Nome: {{ nome }}</p>
+
+  <input type="number" v-model="idade" />
+  <p>Idade: {{ idade }}</p>
+
+  <input type="email" v-model="email" />
+  <p>E-mail:</p>
+</template>
