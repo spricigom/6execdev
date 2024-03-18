de176d7f76f7a13c2f893458bd2109c8fe0edf5a
spricigom <emilispricigo.ifc@gmail.com>
2024-03-18 16:34:22 -0300

    atividade concluida

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,44 @@
 <script setup>
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 
-const nome = ref('')
-const idade = ref('')
-const email = ref('')
+const nome = ref('');
+const idade = ref('');
+const email = ref('');
+const mensagem = ref(false);
+
+function validarforms() {
+  if (idade.value < 18 || idade.value > 60) {
+    alert("A idade deve ser entre 18 e 60 anos");
+    return false;
+  }
+  if (!email.value.includes("@")) {
+    alert("O e-mail deve ser válido.");
+    return false;
+  }
+  return true;
+}
+function enviarforms() {
+  if (validarforms()) {
+    mensagem.value = true;
+  }
+}
 </script>
 <template>
-  <input type="text" v-model="nome" />
+  <input type="text" v-model="nome" maxlength="20" minlength="3">
   <p>Nome: {{ nome }}</p>
 
-  <input type="number" v-model="idade" />
+  <input type="number" v-model="idade">
   <p>Idade: {{ idade }}</p>
 
-  <input type="email" v-model="email" />
-  <p>E-mail:</p>
+  <input type="email" v-model="email">
+  <p>E-mail: {{ email }}</p>
+
+  <button @click="enviarforms">Enviar</button>
+  <p v-if="mensagem"> Dados enviados:</p>
+  <ul v-if="mensagem">
+    <li>Nome: {{ nome }}</li>
+    <li>Idade: {{ idade }}</li>
+    <li>E-mail: {{ email }}</li>
+  </ul>
+
 </template>
